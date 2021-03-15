d81ef50ac0abcc6893fdd753d9fe46e2f2cdaa6f
abhishek1998 <raturiabhi1000@gmail.com>
2021-03-15 00:21:17 -0400

    mongodb populated so that feature page doesnot look empty

--- config/mongoose.js
+++ config/mongoose.js
@@ -18,6 +18,85 @@ module.exports = function() {
 	require('../app/models/user.server.model');
 	require('../app/models/fashionpost.server.model');
 
+    var conn = mongoose.connection;
+
+    //seed the database when it loads for the first time
+    conn.once('open', function() {
+        console.log("conn once")
+        console.log(conn.collections.fashions.find({}));
+
+        var FashionPost = mongoose.model('Fashion');
+
+        var post1 = new FashionPost({
+            category: 'afropunk',
+            price:100,
+            rating:100,
+            brand:"Hugo Boss",
+            size:"M",
+            img: "afropunk.jpeg"
+        });
+
+        post1.save(function(err, post) {
+            if(err) { console.log("post couldnt be created");}
+            else { console.log("post seeded to the mongodb");}
+        });
+
+        var post2 = new FashionPost({
+            category: 'bohemian',
+            price:100,
+            rating:100,
+            brand:"Hugo Boss",
+            size:"M",
+            img: "boss-5.jpeg"
+        });
+
+        post2.save(function(err, post) {
+            if(err) { console.log("post couldnt be created");}
+            else { console.log("post seeded to the mongodb");}
+        });
+
+        var post3 = new FashionPost({
+            category: 'afropunk',
+            price:100,
+            rating:100,
+            brand:"Forever 21",
+            size:"M",
+            img: "forever-21-3.jpeg"
+        });
+
+        post3.save(function(err, post) {
+            if(err) { console.log("post couldnt be created");}
+            else { console.log("post seeded to the mongodb");}
+        });
+        var post4 = new FashionPost({
+            category: 'afropunk',
+            price:100,
+            rating:100,
+            brand:"Hugo Boss",
+            size:"M",
+            img: "boss-2.jpeg"
+        });
+
+        post4.save(function(err, post) {
+            if(err) { console.log("post couldnt be created");}
+            else { console.log("post seeded to the mongodb");}
+        });
+        var post5 = new FashionPost({
+            category: 'afropunk',
+            price:100,
+            rating:100,
+            brand:"Forever 21",
+            size:"M",
+            img: "foreever-21-1.jpeg"
+        });
+
+        post5.save(function(err, post) {
+            if(err) { console.log("post couldnt be created");}
+            else { console.log("post seeded to the mongodb");}
+        });
+
+    });
+
 	// return the mongoose connection instance
 	return db;
 };
