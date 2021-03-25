2dc8530b5e4754f608eba6e79c162114558adf26
abhishek1998 <raturiabhi1000@gmail.com>
2021-03-25 19:40:23 -0400

    changed port

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ const passport = configurePassport();
 
 
 // Use the Express application instance to listen to the '3000' port
-app.listen( process.env.PORT ||  3000);
+app.listen( process.env.PORT ||  5000);
 
 // Log the server status to the console
-console.log('Server running at http://localhost:3000/');
+//console.log('Server running at http://localhost:3000/');
 
 // Use the module.exports property to expose our Express application instance for external usage
 module.exports = app;
